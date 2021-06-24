6fb1aba6148c91a5bff24e49abd625ce618eea87
Tyler Nijmeh <tylernij@gmail.com>
2021-06-24 15:39:38 -0700

    Initial commit
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/powerbuttonsettings/repositories/constants/SettingsConstants.kt
@@ -0,0 +1,6 @@
+package com.draco.powerbuttonsettings.repositories.constants
+
+object SettingsConstants {
+    const val POWER_BUTTON_LONG_PRESS =         "power_button_long_press"
+    const val POWER_BUTTON_VERY_LONG_PRESS =    "power_button_very_long_press"
+}
